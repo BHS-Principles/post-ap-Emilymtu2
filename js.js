59e27954829d4c20f2d9e3345b53e6084d203026
Emilymtu2 <2015220@apps.nsd.org>
2025-05-22 17:05:40 +0000

    feeding family again

--- js.js
+++ js.js
@@ -1,13 +1,24 @@
-var LIST = [
+var LISTPOP = [
     "Carrot", 
     "Dubiously Carrot", 
     "Dubooby Carrot"
 ];
 
+
+for(var i = 0; i < LISTPOP.length; i++) {
+    alert("Check ME OUT!!!!!" + LISTPOP[i]);
+}
+
+var BUTTONS = [
+    "yur",
+    "mom"
+];
+
 var BTN = document.getElementById("myButton");
 
-for(var i = 0; i < LIST.length; i++) {
+for (var j = 0; j < BUTTONS.length; j++) {
     var newBTN = BTN.cloneNode(true);
+    newBTN.textContent = BUTTONS[j];
+    newBTN.id = "button-" + j; 
     document.body.append(newBTN);
-    alert("Check ME OUT!!!!!" + LIST[i]);
-}
+};
